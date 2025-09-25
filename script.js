b092ef942c3a668546ef384ebcb2929ebb984aac
Ionita Aurel Mihai <ionitaaurel32@gmail.com>
2025-09-25 19:23:54 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const cards = document.querySelectorAll(".card");
+cards.forEach((card) => {
+  card.addEventListener("mousemove", (e) => {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = (y - centerY) / 10;
+    const rotateY = (x - centerX) / 10;
+
+    card.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+
+    card.addEventListener("mouseleave", () => {
+      card.style.transform = "rotateX(0deg) rotateY(0deg)";
+    });
+  });
+});
